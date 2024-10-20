f079f6f3a50ec93b15f7803ff04bfd7a65ac0650
Mostafa Elhemali <emostafa@amazon.com>
2024-10-20 23:10:37 +0000

    Bench with subscribers

--- benches/my_benchmark.rs
+++ benches/my_benchmark.rs
@@ -1,12 +1,12 @@
 use criterion::{criterion_group, criterion_main, Criterion};
-use repro_tracing::LocalConnection;
+use repro_tracing::{setup_tracing_one_subscriber, setup_tracing_two_subscribers, LocalConnection};
 
-pub fn criterion_benchmark(c: &mut Criterion) {
+pub fn no_subscriber(c: &mut Criterion) {
     let runtime = tokio::runtime::Builder::new_multi_thread()
         .enable_all()
         .build()
         .unwrap();
-    c.bench_function("work", move |b| {
+    c.bench_function("base_conn", move |b| {
         let connection = runtime.block_on(LocalConnection::connect());
         b.to_async(&runtime).iter(|| async {
             connection.work().await;
@@ -14,5 +14,33 @@ pub fn criterion_benchmark(c: &mut Criterion) {
     });
 }
 
-criterion_group!(benches, criterion_benchmark);
+pub fn one_subscriber(c: &mut Criterion) {
+    let runtime = tokio::runtime::Builder::new_multi_thread()
+        .enable_all()
+        .build()
+        .unwrap();
+    let _outputs = setup_tracing_one_subscriber();
+    c.bench_function("one_sub_conn", move |b| {
+        let connection = runtime.block_on(LocalConnection::connect());
+        b.to_async(&runtime).iter(|| async {
+            connection.work().await;
+        });
+    });
+}
+
+pub fn two_subscriber(c: &mut Criterion) {
+    let runtime = tokio::runtime::Builder::new_multi_thread()
+        .enable_all()
+        .build()
+        .unwrap();
+    let _outputs = setup_tracing_two_subscribers();
+    c.bench_function("two_sub_conn", move |b| {
+        let connection = runtime.block_on(LocalConnection::connect());
+        b.to_async(&runtime).iter(|| async {
+            connection.work().await;
+        });
+    });
+}
+
+criterion_group!(benches, no_subscriber, one_subscriber, two_subscriber);
 criterion_main!(benches);
--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,57 @@
-use std::net::SocketAddr;
+use std::{fs::File, net::SocketAddr, sync::Arc};
 
 use local_ip_address::local_ip;
+use tempfile::{tempdir, TempDir};
 use tokio::{
     io::{AsyncReadExt, AsyncWriteExt},
     net::TcpListener,
 };
+use tracing::Subscriber;
+use tracing_subscriber::{fmt, layer::SubscriberExt, registry, util::SubscriberInitExt, Layer};
+
+pub struct Outputs {
+    output_dir: TempDir,
+    counter: usize,
+    pub outputs: Vec<Arc<File>>,
+}
+
+impl Outputs {
+    fn new(capacity: usize) -> Self {
+        Outputs {
+            output_dir: tempdir().unwrap(),
+            counter: 0,
+            outputs: Vec::with_capacity(capacity),
+        }
+    }
+
+    fn new_layer<S>(&mut self) -> impl Layer<S>
+    where
+        S: Sized + Subscriber + for<'a> tracing_subscriber::registry::LookupSpan<'a>,
+    {
+        let output = Arc::new(
+            File::create_new(self.output_dir.path().join(format!("{}", self.counter))).unwrap(),
+        );
+        self.counter += 1;
+        self.outputs.push(output.clone());
+        let layer = fmt::layer().with_writer(output);
+        layer
+    }
+}
+
+pub fn setup_tracing_one_subscriber() -> Outputs {
+    let mut outputs = Outputs::new(1);
+    registry().with(outputs.new_layer()).set_default();
+    outputs
+}
+
+pub fn setup_tracing_two_subscribers() -> Outputs {
+    let mut outputs = Outputs::new(1);
+    registry()
+        .with(outputs.new_layer())
+        .with(outputs.new_layer())
+        .set_default();
+    outputs
+}
 
 const BUF_SIZE: usize = 1024;
 
